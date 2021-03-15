64311534b10e307de503fdebeceb85395e65cfdd
Vaislou <vaislou@gmail.com>
2021-03-15 20:39:23 +0300

    fix: complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsConfigArr = bracketsConfig.flatMap(call => call.join(''));
+    if (str.length % 2 !== 0) {
+        return false;
+    } else {
+        for (let i = 0; i < bracketsConfigArr.length; i++) {
+            if (str.includes(bracketsConfigArr[i])) {
+                str = str.replace(bracketsConfigArr[i], '')
+                i -= bracketsConfigArr.length;
+            }
+        }
+        if (str == 0) {
+            return true;
+        } else {return false;}
+    }
 }
